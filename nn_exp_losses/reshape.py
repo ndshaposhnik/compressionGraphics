0870809b7c03ae3cfb8a59944d255664bea6d502
Nikita Shaposhnik <nikita.shaposhnik@gmail.com>
2023-12-26 02:08:39 +0300

    update

--- nn_exp_losses/reshape.py
+++ nn_exp_losses/reshape.py
@@ -1,8 +1,19 @@
-file = input()
+import sys
+import os.path
 
-with open(file, "r") as f:
-    losses = f.readline()
-    lines = list(map(float, losses.split()))
-    with open(file, "w") as out:
+def process_file(filepath, dtype=float):
+    with open(filepath, "r") as f:
+        losses = f.readline()
+        lines = list(map(dtype, losses.split()))
+    with open(filepath, "w") as out:
         for line in lines:
             print(line, file=out)
+
+def main(argv):
+    folder = argv[0]
+    process_file(os.path.join(folder, 'loss.txt'))
+    process_file(os.path.join(folder, 'coords.txt'), dtype=int)
+                
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
